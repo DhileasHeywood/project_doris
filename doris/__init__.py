d30070ab06dda60213ee8a00e150c208d4e02909
Dhileas Heywood <dhileas.heywood@gmail.com>
2020-09-30 17:48:47 +0100

    .gitignore updated to include flask. Attempting to write tests

---
+++ doris/__init__.py
@@ -0,0 +1,32 @@
+import os
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev"
+        #DATABASE= Something goes here, but I'm not sure what yet.
+    )
+
+    if test_config is None:
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    return app
+
+app = create_app()
+
+@app.route("/hello")
+def hello():
+    greeting = "Hello, World!"
+    return greeting
+
+if __name__ == "__main__":
+    app.run()
--- doris/test/unit/test_user_login.py
+++ doris/test/unit/test_user_login.py
@@ -1,6 +1,12 @@
+import pytest
+from doris import auth
+
+
+def test_hello():
+    assert greeting == "Hello, World!"
+
 # save new user details to database
 
 # retrieve details from database
 
 # authenticate details input by user
-
